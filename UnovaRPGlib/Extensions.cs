e31ab05db50b49f1ef09da3e44458441e8abb3cb
HoLLy <dario.db@hotmail.com>
2017-11-07 13:35:29 +0100

    Add Xajax helper method

---
+++ UnovaRPGlib/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnovaRPGlib
+{
+    internal static class Extensions
+    {
+        public static string Xajax(this WebClient wc, string url, string function)
+        {
+            long time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            byte[] resp = wc.UploadValues(url, new NameValueCollection {
+                {"xjxfun", function},
+                {"xjxr", time.ToString()}
+            });
+
+            return Encoding.UTF8.GetString(resp);
+        }
+    }
+}
--- UnovaRPGlib/UnovaSession.cs
+++ UnovaRPGlib/UnovaSession.cs
@@ -39,14 +39,7 @@ namespace UnovaRPGlib
 
         public void Heal()
         {
-            long dong = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-
-            byte[] resp = _web.UploadValues(Urls.UrlPokemonCenter, new NameValueCollection {
-                {"xjxfun", "recoverMyPokemon"},
-                {"xjxr", dong.ToString()}
-            });
-            string respS = Encoding.UTF8.GetString(resp);
-            Console.WriteLine(respS);
+            string resp = _web.Xajax(Urls.UrlPokemonCenter, "recoverMyPokemon");
         }
     }
 }
